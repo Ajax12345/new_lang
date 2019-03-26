a49bc04501a79650d2e7264fb9e6dc32a9d0ae6b
James Petullo <pentester18@gmail.com>
2019-03-25 23:56:08 -0400

    Update main_parser.py

--- new_lang/main_parser.py
+++ new_lang/main_parser.py
@@ -78,7 +78,7 @@ class _AST_op(parser_Header):
                 raise lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Syntax: unexpected termination of expression")
             if _attr.name != 'variable':
                 raise lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Syntax: '{attr.value}'")
-            yield _attr.value
+            yield _attr
             _c = next(self.header, None)
         if _c is not None:
             self.header = iter([_c, *self.header])
@@ -87,39 +87,40 @@ class _AST_op(parser_Header):
         """
         todo: add support for in-place value mutations
         """
+        def to_dict(_ast):
+            return _ast if _ast.name == 'running_name' else {'op':_ast.op, 'left':to_dict(_ast.left), 'right':to_dict(_ast.right)}
+        _path = [self.start_token]
         _identifier, path = next(self.header, None), None
-        if _identifier.name not in {'colon', 'dot', 'lparent', 'assign'}:
+        if _identifier.name not in {'colon', 'dot', 'lparent', 'assign', 'increment', 'decrement', 'inplace'}:
             raise lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Syntax: invalid syntax with name '{_identifier.value}'")
-        if _identifier.name == 'dot':
-            _path = list(self.attr_lookup(_identifier))
-        
-        elif _identifier.name == 'assign':
-            _full_ast, *_ = ast_creator.AST.create_ast(self.reverse_op, lang_utility_objs.ast_delimeters())
-            if not _full_ast:
-                raise lang_exceptions.InvalidAssigmentTermination(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Termination of Assignment: Expecting valid type")
-            print(_full_ast)
-            print('---'*10)
-            def to_dict(_ast):
-                return _ast if _ast.name == 'running_name' else {'op':_ast.op, 'left':to_dict(_ast.left), 'right':to_dict(_ast.right)}
-            print(to_dict(_full_ast))            
-            '''
-            print('-----')
-            for i in _full_ast:
-                print(i)
-                print('-----')
-            '''
-        elif _identifier.name == 'colon':
-            #build AST to =
-            pass
-        else:
-            raise lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Syntax: invalid syntax with name '{_identifier.value}'")
+        _path.extend(list(self.attr_lookup(_identifier)))
+        _identifier = next(self.header, None)
+        if _identifier.name in {'assign', 'inplace'}:
+            _full_ast, *_ = ast_creator.AST.create_ast(self.reverse_op, lang_utility_objs.ast_delimeters())          
+            return lang_utility_objs.NameAction(_path, None, _identifier, _full_ast)          
+        if _identifier.name == 'colon':
+            _type_ast, _, _last = ast_creator.AST.create_ast(self.reverse_op, lang_utility_objs.ast_delimeters("assign"))
+            self.header = iter([_last, *self.header])
+            _check_next = next(self.header, None)
+            if _check_next is None:
+                raise lang_exceptions.InvalidTypeDeclaration(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Type Declaration: expecting assignment expression")
+            if _check_next.name != 'assign':
+                lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Assignment Expression: expecting value assignment")
+            _assignment_ast, *_ = ast_creator.AST.create_ast(self.reverse_op, lang_utility_objs.ast_delimeters())
+            return lang_utility_objs.NameAction(_path, _type_ast, _check_next, _assignment_ast)
+        if _identifier.name in {'increment', 'decrement'}:
+            _check_next = next(self.header, None)
+            if _check_next is not None:
+                raise lang_exceptions.InvalidInplaceExpression(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Inplace Expression: invalid {_identifier.name} expression")
+            return lang_utility_objs.InPlaceAction(_path, _identifier)        
+        raise lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_counter.line_number}:\nInvalid Syntax: invalid syntax with name '{_identifier.value}'")
 
     
     @classmethod
     def get_obj_ast(cls, _start_token, _parser_obj, _header:typing.Iterator, _lines:typing.Iterator, _line_counter:LineCount, _file:str, _current_level:int, scope:Scope) -> typing.Any:
         _main_execute = cls(_start_token, _parser_obj, _header, _lines, _line_counter, _file, _current_level, scope)
-        _main_execute.start()
-        return 
+        return _main_execute.start()
+        
 
 class Parser:
     def __init__(self, _file:str, scope:Scope, _whitespace:int = 0, _line_count = 1) -> None:
@@ -147,8 +148,9 @@ class Parser:
                 _trailing = iter(_trailing)
             if _t.token_type not in self.token_matcher:
                raise lang_exceptions.InvalidSyntax(f"In '{self.file}', line {self.line_count.line_number}:\nInvalid Syntax: invalid identifier '{_t.value}'") 
-            _ = self.token_matcher[_t.token_type].get_obj_ast(_t, self.__class__, _trailing, self.stream, self.line_count, self.file, self.level, self.scope)
-
+            executable_ast = self.token_matcher[_t.token_type].get_obj_ast(_t, self.__class__, _trailing, self.stream, self.line_count, self.file, self.level, self.scope)
+            print('got executable ast: ', executable_ast)
+            print('-'*20)
             _start = next(self, None)
             next(self.line_count)
 
