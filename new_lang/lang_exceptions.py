d1ba668a90b27268bbd0e7923433c4c9b75c5851
James Petullo <pentester18@gmail.com>
2019-03-25 23:55:44 -0400

    Update lang_exceptions.py

--- new_lang/lang_exceptions.py
+++ new_lang/lang_exceptions.py
@@ -7,3 +7,15 @@ class InvalidSyntax(Exception):
 class InvalidIdentifier(Exception):
     pass
 
+class InvalidFunctionName(Exception):
+    pass
+
+class InvalidAssigmentTermination(Exception):
+    pass
+
+class InvalidTypeDeclaration(Exception):
+    pass
+
+class InvalidInplaceExpression(Exception):
+    pass
+
